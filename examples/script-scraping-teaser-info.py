3e8fae3893db7f99d532997d17843331f358f169
Malte Sauerwein <malte.sauerwein@wipro.com>
2023-02-02 16:06:26 +0100

    examples updated

--- examples/script-scraping-teaser-info.py
+++ examples/script-scraping-teaser-info.py
@@ -10,9 +10,9 @@ import logging
 import time
 import os
 from datetime import datetime
-from tagesschauscraper import constants, helper, tagesschau
+from tagesschauscraper import helper, tagesschau
 
-# Arguemnt parsing
+# Argument parsing
 parser = argparse.ArgumentParser(
     prog="TagesschauScraper",
     description=(
@@ -36,18 +36,31 @@ parser.add_argument(
     choices=["wirtschaft", "inland", "ausland", "all"],
 )
 parser.add_argument(
+    "--datadir",
+    type=str,
+    help="Output dir",
+    default="data",
+)
+parser.add_argument(
+    "--logdir",
+    type=str,
+    help="Log dir",
+    default="logs",
+)
+
+parser.add_argument(
     "-v", "--verbose", action="store_true", help="Enable verbose output"
 )
 args = parser.parse_args()
 
 # Set up logging
-if not os.path.exists(constants.log_dir):
-    os.makedirs(constants.log_dir)
+if not os.path.exists(args.logdir):
+    os.makedirs(args.logdir)
 log_file = helper.create_file_name_from_date(
     datetime.now(), suffix="scrape", extension=".log"
 )
 logging.basicConfig(
-    filename=os.path.join(constants.log_dir, log_file),
+    filename=os.path.join(args.logdir, log_file),
     level=logging.DEBUG if args.verbose else logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
@@ -57,26 +70,31 @@ start_time = time.time()
 
 input_date_pattern = "%Y-%m-%d"
 date_ = datetime.strptime(args.date, input_date_pattern).date()
-category = args.category
 
 logging.info(
     f"Initialize scraping for date {args.date} and category {args.category}"
 )
 tagesschauScraper = tagesschau.TagesschauScraper()
-url = tagesschau.create_url_for_news_archive(date_, category=category)
+url = tagesschau.create_url_for_news_archive(date_, category=args.category)
 logging.info(f"Scraping teaser from URL {url}")
 teaser = tagesschauScraper.scrape_teaser(url)
 logging.info("Scraping terminated.")
 
-dateDirectoryTreeCreator = helper.DateDirectoryTreeCreator(date_)
-file_path = os.path.join(
-    dateDirectoryTreeCreator.file_path,
+if not os.path.isdir(args.datadir):
+    os.mkdir(args.datadir)
+dateDirectoryTreeCreator = helper.DateDirectoryTreeCreator(
+    date_, root_dir=args.datadir
+)
+file_path = dateDirectoryTreeCreator.create_file_path_from_date()
+dateDirectoryTreeCreator.make_dir_tree_from_file_path(file_path)
+file_name_and_path = os.path.join(
+    file_path,
     helper.create_file_name_from_date(
-        date_, suffix="_" + category, extension=".json"
+        date_, suffix="_" + args.category, extension=".json"
     ),
 )
-logging.info(f"Save scraped teaser to file {file_path}")
-helper.save_to_json(teaser, file_path)
+logging.info(f"Save scraped teaser to file {file_name_and_path}")
+helper.save_to_json(teaser, file_name_and_path)
 logging.info("Done.")
 end_time = time.time()
 logging.info(f"Execution time: {end_time - start_time:.2f} seconds")
